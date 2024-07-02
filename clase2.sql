2a669e8e11f43cd9c1d78c881beff5b7e3a74518
AleMMb <alejandramellab@gmail.com>
2024-07-01 20:44:20 -0400

    normalization

--- clase2.sql
+++ clase2.sql
@@ -27,3 +27,29 @@ CREATE DATABASE clase_practica
 -- TCL: Transaction Control Language.
 
 -- DBA: Database Administrator --> define estructuras, perfiles de seguridad, respaldo, implemntar integridar, definir procedimeintos, monitorear.
+
+
+-- Clase 4 01/07/2024
+
+--INICIO:
+(ID_orden, fecha, id_cliente, nom_cliente, estado, num_art, nom_art, cant, precio)
+
+-- Primera forma formal. 
+-- Eliminar grupos o datos repetidos.
+-- 0 datos duplicados y datos atomicos.
+
+Ordenes: {ID_orden (pk), fecha, id_cliente, nom_cliente, estado}
+Detalle: {ID_detalle (pk), ID_orden(fk), num_art, nom_art, cant, precio}
+
+--Segunda forma normal.
+-- Eliminacion de las dependencias parciales. (un valor no puede depender de la PK o parte de ella)
+Ordenes: {ID_orden (pk), fecha, id_cliente, nom_cliente, estado}
+Detalle: {ID_detalle (pk), ID_orden(fk), num_art, nom_art, cant, precio}
+--explicacion: no hay dependencias parciales en ninguna de las tablas.
+
+--Tercera forma normal.
+-- Eliminacion de las dependencias transitivas. (cada taributo debe depender solamente de la PK)
+Ordenes: {ID_orden (pk), fecha, id_cliente(fk)}
+Cliente: {id_cliente(pk), nom_cliente, estado}
+Detalle: {ID_detalle (pk), ID_orden(fk), num_art(fk), cant}
+Articulos: {num_art(pk), nom_art, precio}
